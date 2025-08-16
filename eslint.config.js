bbad5dbb3901ab2abb265f4e268c437b84b140b7
Mohammed Alrabrabah <m7md1alaa15@outlook.com>
2025-08-16 11:54:21 +0300

    refactor: update ESLint configuration, add Tailwind CSS, and clean up App component

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,20 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   react: true,
-  stylistic: true,
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
   typescript: true,
-
+  unicorn: {
+    overrides: {
+      'unicorn/filename-case': [
+        'error',
+        {
+          case: 'kebabCase',
+          ignore: ['^.*\\.d\\.ts$'],
+        },
+      ],
+    },
+  },
 })
