319a7f0cee7290c0d55b5cc8b8fa50a4fe29da94
Mohammed Alrabrabah <m7md1alaa15@outlook.com>
2025-09-01 14:20:14 +0300

    feat: add api hono remove bun and use pnpm again due to binrary modules prolems

---
+++ apps/api/eslint.config.js
@@ -0,0 +1,3 @@
+import createConfig from '@guess-logo/eslint-config/create-config';
+
+export default await createConfig();
--- packages/eslint-config/create-config.js
+++ packages/eslint-config/create-config.js
@@ -31,6 +31,7 @@ export default function createConfig(options, ...userConfigs) {
           },
         ],
       },
+      ignores:["README.md"]
     },
     ...userConfigs,
   },
