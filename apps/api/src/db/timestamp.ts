2a2404dd379a1c354841ac455c13036d9d120da0
Mohammed Alrabrabah <m7md1alaa15@outlook.com>
2025-11-01 04:26:37 +0300

    feat(db): Introduce `five_seconds_categories` and `five_seconds_questions` tables.
    Consolidate previous incremental migrations into a single new file.
    fix `createdAt` and `updatedAt` d1 not support DEFAULT

--- apps/api/src/db/timestamp.ts
+++ apps/api/src/db/timestamp.ts
@@ -4,8 +4,6 @@ import { integer } from 'drizzle-orm/sqlite-core';
 const nowSql = sql`cast((julianday('now') - 2440587.5)*86400000 as integer)`;
 
 export const timestamp = {
-  createdAt: integer({ mode: 'timestamp' }).default(nowSql),
-  updatedAt: integer({ mode: 'timestamp' })
-    .default(nowSql)
-    .$onUpdate(() => nowSql),
+  createdAt: integer({ mode: 'timestamp' }).notNull(),
+  updatedAt: integer({ mode: 'timestamp' }).notNull().$onUpdate(() => nowSql),
 };
