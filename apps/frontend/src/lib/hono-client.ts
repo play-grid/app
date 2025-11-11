7f495a1e0ad2207640e1fe342daf1b5199c00afe
Mohammed Alrabrabah <m7md1alaa15@outlook.com>
2025-11-11 17:28:08 +0300

    feat: enhance game experience with improved error handling and UI updates

--- apps/frontend/src/lib/hono-client.ts
+++ apps/frontend/src/lib/hono-client.ts
@@ -2,6 +2,11 @@ import type { router } from '@guess-logo/api/routes';
 import { hc } from 'hono/client';
 import { env } from '@/env';
 
-const client = hc<router>(env.VITE_API_URL);
+export type Client = ReturnType<typeof hc<typeof router>>;
+
+export function hcWithType(...args: Parameters<typeof hc>): Client {
+  return hc<typeof router>(...args);
+}
+const client = hcWithType(env.VITE_API_URL);
 
 export default client;
