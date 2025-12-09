105294dc38f05803b8b33077f5c12cfee7a5a55f
Mohammed alaa <m7md1alaa15@outlook.com>
2025-12-09 13:34:39 +0300

    chore: refactor pnpm config and update eslint
    Move pnpm `overrides` and `onlyBuiltDependencies` from `package.json` to `pnpm-workspace.yaml`.
    Update ESLint configuration to disable `pnpm/json-enforce-catalog`. to follow antfu/pnpm-workspace-utils rules

--- packages/eslint-config/create-config.js
+++ packages/eslint-config/create-config.js
@@ -1,35 +1,34 @@
-import antfu from "@antfu/eslint-config";
+import antfu from '@antfu/eslint-config';
 
 export default function createConfig(options, ...userConfigs) {
   return antfu({
-    type: "app",
-    // Enable stylistic formatting rules
+    type: 'app',
     stylistic: {
       indent: 2,
       semi: true,
-      quotes: "single",
+      quotes: 'single',
     },
     ...options,
-    // TypeScript and Vue are autodetected, you can also explicitly enable them:
     typescript: true,
 
-    // Disable jsonc and yaml support
     jsonc: false,
     yaml: false,
-
-    ignores: ["**/fixtures", "node_modules", "dist", "build", "**/*.md"],
-
+    ignores: ['**/fixtures', 'node_modules', 'dist', 'build', '**/*.md'],
+    rules: {
+      'pnpm/json-enforce-catalog': 'off',
+    },
     unicorn: {
       overrides: {
-        "unicorn/filename-case": [
-          "error",
+        'unicorn/filename-case': [
+          'error',
           {
-            case: "kebabCase",
-            ignore: ["^.*\\.d\\.ts$", "README\\.md"],
+            case: 'kebabCase',
+            ignore: ['^.*\\.d\\.ts$', 'README\\.md'],
           },
         ],
       },
     },
     ...userConfigs,
-  });
+  },
+  );
 }
